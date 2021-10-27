11a1e1623c21dccdf95ad8c3ba757bff7bd62bc7
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2021-10-27 21:25:21 +0200

    ok

--- ElienVader.cpp
+++ ElienVader.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
+#include <box2d/box2d.h>
 
-
-
-
-int main()
+int main(int argc, char* argv[])
 {
-    
+        
+	b2Vec2 gravity(0.0f, -10.f);
+	b2World world(gravity);
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0.0f, -10.0f);
+
+	return 0;
 }
