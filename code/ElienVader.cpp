ab179e25cd27a73c761b60996fcbfcbeb79b3bec
Hevagog <qkawiak@gmail.com>
2021-11-10 23:41:05 +0100

    Wstępna klasa gracza

--- code/ElienVader.cpp
+++ code/ElienVader.cpp
@@ -1,7 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+class Player{
+        float speed,positionX;
+        int sizeX,lives;
+    public:
+        Player()
+        {   
+            lives=3;
+            sizeX = 100;
+            speed = 5.0f;
+            positionX = 0.f;
+        }
+
+        void check_input();
+        void move_player();
+        void display_player();
+
+};
+
+void Player::display_player()
+{
+    sf::RectangleShape player({100, 50});
+    
+}
+
+void Player::check_input()
+{
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)))
+    {
+        std::cout<<"Aa"<<std::endl;
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)))
+    {
+        std::cout<<"BBB"<<std::endl;
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+    {
+        
+    }
+}
 
 int main()
 {
+    Player player;
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setPosition({0, 200});
@@ -23,7 +65,8 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        player.check_input();
+        player.display_player();
         window.clear();
         window.draw(shape);
         window.draw(shape1);
