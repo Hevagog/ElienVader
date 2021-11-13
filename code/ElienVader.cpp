83291a67972eed17512d5ac2e208c0c64a2bb78c
Hevagog <qkawiak@gmail.com>
2021-11-13 01:06:46 +0100

    Update ElienVader.cpp

--- code/ElienVader.cpp
+++ code/ElienVader.cpp
@@ -1,24 +1,65 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+class Projectile : public sf::Drawable{
+        float speed,radius,dmg;
+        int positionY,positionX;
+        bool side;
+    public:
+        Projectile(float damage,int initial_position,bool is_my_side)
+        {
+            positionX = initial_position;
+            positionY = 0;
+            speed = 18.0f;
+            radius = 5.0;
+            dmg = damage;
+            side = is_my_side;
+        }
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+        void move();// maybe move to draw function??
+        void impact(); // _logic & functionality TBD_
+};
 
+void Projectile::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    sf::CircleShape p(radius);
+    p.setPosition(positionX,positionY);
+    target.draw(p,states);
+}
+
+void Projectile::move()
+{
+    if(side)
+        positionY += 1.0*speed;
+    else
+        positionY -= 1.0*speed;
+}
+void Projectile::impact()
+{
+    //if(positionY>=1000||positionY<=0)
+}
 
 class Player : public sf::Drawable {
         float speed;
         int lives;
+        bool cooldown;
     public:
         float sizeX,positionX;
+        bool shot;
         Player()
         {   
-            lives=3;
+            shot  = 0;
+            lives = 3;
             sizeX = 50;
             speed = 20.0f;
             positionX = 500-sizeX/2;
+            cooldown=1;//TO BE MODIFIED
         }
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
         void process_events(sf::Event& event);
         void check_colision(int width);
         void process_event(sf::Event event);
+        //void check_impact();//cooldown starts from impact ~1 
 };
 
 void Player::draw(sf::RenderTarget& target, sf::RenderStates states) const
@@ -37,8 +78,8 @@ void Player::process_event(sf::Event event)
         if (event.key.code == sf::Keyboard::Key::D) {
             positionX+=1*speed;
         }
-        if (event.key.code == sf::Keyboard::Key::Space) {
-            //Do something
+        if (event.key.code == sf::Keyboard::Key::Space && cooldown) {
+            shot=1;            
         }
     }
 }
@@ -57,12 +98,14 @@ class Enemy : public sf::Drawable, public Player{
     //TODO
 };
 
+
+
 class Game
 {
     private:
         static const int window_width  = 1000;
         static const int window_height = 1000;
-        sf::RenderWindow window;
+        sf::RenderWindow window; 
         Player player;
     public:
         Game(): window(sf::VideoMode(1000,1000),"ElienVader", sf::Style::Close)
@@ -74,7 +117,6 @@ class Game
 
 
 void Game::run(){
-    
     while (window.isOpen())
     {
         process_event();
